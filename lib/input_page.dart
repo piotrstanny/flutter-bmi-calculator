5ffc63aa6d92a0fafd994104108a330b813a9712
Piotr Stanny <piotr.stanny@gmail.com>
2020-08-08 19:56:20 +0100

    Seperate stateful widget as a different file

---
+++ lib/input_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Center(
+        child: Text('Bodyfff text'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,28 +13,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0E23),
         scaffoldBackgroundColor: Color(0xFF0A0E23),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body text'),
-      ),
     );
   }
 }
